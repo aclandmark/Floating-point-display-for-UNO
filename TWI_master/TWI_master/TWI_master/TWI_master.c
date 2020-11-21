7f25a397f5c3a91a331cf70e43dcef169b5707b4
aclandmark <mark.osborne15@btinternet.com>
2020-11-21 18:11:53 +0000

    Update TWI_master.c

--- TWI_master/TWI_master/TWI_master/TWI_master.c
+++ TWI_master/TWI_master/TWI_master/TWI_master.c
@@ -7,8 +7,7 @@ void USI_TWI_Master_Initialise(void);
 long string_to_binary(char *);
 
 
-volatile int buf_ptr;
-volatile char int_counter;
+
 
 int main (void){
 
@@ -17,19 +16,19 @@ setup_ATtiny_HW;
 set_digit_drivers;
 clear_digits;
 clear_display;	
-TCCR0A |= 1 << TCW0;								//16 bit mode
-OCR0B = 0x1;
+TCCR0A |= 1 << TCW0;												//Timer0 in 16 bit mode
+OCR0B = 0x1;														//Used to control intensity
 OCR0A = 0;
-TCNT0H = 0x7F;
+TCNT0H = 0x7F;														//Generates 4mS interrupt stream 
 TCNT0L = 0xFF;
-TIMSK |= (1 << TOIE0) | (1 << OCIE0A);				//Initialise Timer interrupts
-data = 0;
-int_counter = 0;
-//digit_3; 
-sei();
+TIMSK |= (1 << TOIE0) | (1 << OCIE0A);								//Initialise Timer interrupts
+
+int_counter = 0;													//T0 overflow interrupt counter
+ 
+sei();																//Required by display and TWI
 
 USI_TWI_Master_Initialise();
-while (!(send_save_address_plus_RW_bit(0x6)));	//three;			//master writes to slave
+while (!(send_save_address_plus_RW_bit(0x6)));						//master writes to slave
 for(int m = 0; m <= 4; m++){
 	if (m==4)write_data_to_slave(4, 1);
 	else write_data_to_slave(m, 0);	}
@@ -39,28 +38,18 @@ for(int m = 5; m <= 9; m++){
 else write_data_to_slave(m, 0);	}
 
 
+TCCR0B = 1;																//Start 4mS Timer0 clock:TWI ready to receive binary or string data 
 while(1){
-TCCR0B = 1;
-while (!(cr_keypress));
-cr_keypress = 0;
+while (!(cr_keypress));													//Wait here for TWI interrupts. 
+cr_keypress = 0;														//String received from UNO: Clear carriage return 
 
-Number = 0;
-/*for(int m = 0; m <= 3; m++){
-if(display_buf[m]){	
-Number = Number*10 + (display_buf[m] - '0');}}*/
-Number = string_to_binary(display_buf);
+Number = string_to_binary(display_buf);									//Convert the string to a binary number
 
-
-/*{Number = display_buf[0] - '0';												//Convert the string to a number
-	Number = Number*10 + (display_buf[1] - '0');									//But only after a return key press
-	Number = Number*10 + ( display_buf[2] - '0');
-Number = Number*10 + ( display_buf[3] - '0');}*/
-
-while (!(send_save_address_plus_RW_bit(0x6)));							//Data sent to UNO
-	for(int m = 0; m <= 3; m++){												//BUT only after a return key press
+while (!(send_save_address_plus_RW_bit(0x6)));							//Return the number to the UNO
+	for(int m = 0; m <= 3; m++){
 		if(m == 3)write_data_to_slave(Number, 1);
 	else write_data_to_slave(Number >> (8*(3-m)), 0);}
-}
+}																		//If binary data is received, display it but return nothing to the UNO
 
 
 
@@ -68,14 +57,14 @@ while (!(send_save_address_plus_RW_bit(0x6)));							//Data sent to UNO
 
 while(1);
 
-while(1){
+/*while(1){
 USI_TWI_Master_Initialise();
 for(int m = 0; m < 4; m++)
 {while(!(data));TCCR0B = 0;							//Pause clock for I2C transaction
 	display_buf[m] = data; data = 0;}
 USICR = 0;											//Put I2C on hold
 TCCR0B = 1;	}										//Re-start 8MHz clock
-
+*/
 
 
 wdt_enable(WDTO_60MS); while(1);}
@@ -83,17 +72,18 @@ wdt_enable(WDTO_60MS); while(1);}
 
 
 /******************************************************************************************************/
-ISR (TIMER0_OVF_vect){TCNT0H = 0x7F;	//TCCR0B = 0;				//Generates interrupt every 4.096mS.
+ISR (TIMER0_OVF_vect){TCNT0H = 0x7F;				//Generates interrupt every 4.096mS.
 	TCNT0L = 0xFF;
 Display_driver();
 int_counter ++;
-if (int_counter == 10){int_counter = 0; data_from_UNO(); }
+if (int_counter == 10)
+{int_counter = 0; data_from_UNO(); }				//Polls UNO every 41mS
 }
 
 
 
 /******************************************************************************************************/
-void Display_driver()
+void Display_driver()								//Display multiplexer advances every 4mS							
 {buf_ptr++; buf_ptr = buf_ptr%4;
 	clear_digits;
 	clear_display;
@@ -108,8 +98,8 @@ void Display_driver()
 
 
 /******************************************************************************************************/
-	ISR (TIMER0_COMPA_vect){
-	clear_display;}
+	ISR (TIMER0_COMPA_vect){						//Controls display intensity
+	clear_display;}									//by shutting drive current down in less than 4mS.
 
 
 
@@ -136,13 +126,13 @@ void Display_driver()
 long string_to_binary(char array[]){
 	
 	char sign = '+';
-	long number = 0;
+	long num = 0;
 
 	for(int m = 0; m <= 3; m++){
 		if(array[m]){
 			if(array[m] == '-'){sign = '-'; continue;}
-		number = number*10 + (array[m] - '0');}}
-	if (sign == '-')number *= (-1);
-	return number;}
+		num = num*10 + (array[m] - '0');}}
+	if (sign == '-')num *= (-1);
+	return num;}
 
 
