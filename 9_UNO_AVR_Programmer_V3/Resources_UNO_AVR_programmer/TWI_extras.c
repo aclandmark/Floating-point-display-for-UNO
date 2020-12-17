39f67dc78b482de09990c92c2b01805f4db31961
aclandmark <mark.osborne15@btinternet.com>
2020-12-17 11:24:57 +0000

    Update TWI_extras.c

--- 9_UNO_AVR_Programmer_V3/Resources_UNO_AVR_programmer/TWI_extras.c
+++ 9_UNO_AVR_Programmer_V3/Resources_UNO_AVR_programmer/TWI_extras.c
@@ -151,47 +151,26 @@ return keypress;}
 
 /***************************************************************************************************************************************/
 float Float_from_KBD(char digits[]){                                   //Acquires an integer string from the keyboard and returns the binary equivalent
-char keypress;
-float f_number;
-float * Flt_ptr_local;
-char * Char_ptr_local;
+char keypress, dig_ptr=0;
+//float f_number;
 
-Flt_ptr_local = &f_number;
-Char_ptr_local = (char*)&f_number;
 
-cr_keypress = 0;                                                    //Set to one when carriage return keypress terminates the string
+
+//cr_keypress = 0;                                                    //Set to one when carriage return keypress terminates the string
 for(int n = 0; n<=3; n++) digits[n] = 0;                            //Clear the buffer used to the string
 do
 {keypress =  waitforkeypress();} 
 while ((!(decimal_digit(keypress)))
 && (keypress != '-')
 && (keypress != '.'));
-digits[0] = keypress;
-float_string_to_display();                                           //Update display with the first key press
+digits[dig_ptr++] = keypress;
 while(1){
 if ((keypress = wait_for_return_key())  =='\r')break;               //Detect return key press (i.e \r or\r\n)
 if ((decimal_digit(keypress)) || (keypress == '.'))
 {
-if(keypress != '.')
-{for(int n = 3; n>=1; n--)                                          //Shift display for each new keypress except '.'
-digits[n] = digits[n-1];
-digits[0] = keypress;}                                              //Add new keypress           
-else digits[0] |= 0x80;
-float_string_to_display();
+digits[dig_ptr++] = keypress;
 }}                                                                  //Update display includes "cr_keypress"                                                 
-cr_keypress = 1;                                                     //End of string; return key pressed
-float_string_to_display();
-cr_keypress = 0;
-TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWINT);                    //Activate TWI and wait for contact from display pcb 
-while (!(TWCR & (1 << TWINT)));
 
-*Char_ptr_local =  byte(receive_byte_with_Ack());  Char_ptr_local += 1;                      //Build up the number as each byte is received
-*Char_ptr_local =  byte(receive_byte_with_Ack());  Char_ptr_local += 1;  
-*Char_ptr_local =  byte(receive_byte_with_Ack());  Char_ptr_local += 1;      
-*Char_ptr_local =  byte(receive_byte_with_Nack());    
-f_number = *Flt_ptr_local;
-
-TWCR = (1 << TWINT);
-return f_number;}
+return 0;}
 
 
