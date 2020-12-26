7a8a3b9a9a3c2db2274ee2680471facc783b03d8
aclandmark <mark.osborne15@btinternet.com>
2020-12-26 17:59:50 +0000

    Update TWI_extras.c

--- 8_UNO_AVR_Programmer_V2_5/Resources_UNO_AVR_programmer/TWI_extras.c
+++ 8_UNO_AVR_Programmer_V2_5/Resources_UNO_AVR_programmer/TWI_extras.c
@@ -12,6 +12,14 @@ void float_string_to_display(void);
 long Num_from_KBD(char*);
 float Float_from_KBD(char *);
 
+signed char ftoa(float, char*, int);
+void Print_unrounded(char*, char);
+void Round_for_Display(char*, char, signed char);
+
+
+
+
+
 
 
 void Num_to_PC(char radix, long long_num){
@@ -58,26 +66,93 @@ UDR0 = data;}
 
 
 /***************************************************************************************************************************************/
- void ftoa(float Fnum, char FP_string[], int afterpoint){
-		long ipart;
-		char sign = '+';
-		
-		for(int m = 0; m <= 8; m++) FP_string[m] = 0;
-		
-		if (Fnum < 0){sign = '-'; Fnum *= (-1);}
-		
-		ipart = (long)Fnum;
-		float fpart = Fnum - (float)ipart;
-		long i = longToStr(ipart, FP_string, 0);
+signed char ftoa(float Fnum, char FP_string[], int afterpoint){
+  long ipart, Fnum_int;
+  char sign = '+';
+  signed char expt;
+  
+  if (Fnum < 0){sign = '-'; Fnum *= (-1);}                                //Convert negative numbers to positive ones and set the sign character
+  
+  for(int m = 0; m <= 15; m++) FP_string[m] = 0;                          //Clear the floating point array
+  
+  Fnum_int = (long)Fnum;                                                  //Obtain integer part of array
+  if(Fnum_int < 0) Fnum_int = Fnum_int * (-1);                            //Only used to set the number of decimal places 
 
-		if (afterpoint != 0){
-			FP_string[i] = '.';
-			fpart = fpart * pow(10,afterpoint);
-		longToStr((long)fpart, FP_string + i + 1, afterpoint);}
-		
-		if(sign == '-'){for(int m = 0; m <= 7; m++)FP_string[8-m] = FP_string[7-m];
-		FP_string[0] = '-';	
-		}	}
+  if (Fnum_int < 10)afterpoint = 5;                                       //Number of decimal places is matched to display length
+  if ((Fnum_int >= 10) && (Fnum_int < 100))afterpoint = 4;
+  if ((Fnum_int >= 100) && (Fnum_int < 1000))afterpoint = 3;
+  if ((Fnum_int >= 1000) && (Fnum_int < 10000))afterpoint = 2;
+  if ((Fnum_int >= 10000) && (Fnum_int < 100000))afterpoint = 1;
+
+  expt = 0;                                                                 //Convert very large and small numbers to scientific form
+  if (Fnum  >= 10000) { while (Fnum > 10)
+  {Fnum /= 10; expt += 1;}afterpoint = 5;}
+  if (Fnum < 0.01) {while (Fnum < 1){Fnum *= 10; expt -= 1;}}
+  
+                                        
+  ipart = (long)Fnum;                                                       //Obtain integer part of FP number
+  float fpart = Fnum - (float)ipart;                                        //Obtain floating part
+  long i = longToStr(ipart, FP_string, 0);                                  //Convert integer part to string
+
+  if (afterpoint != 0){                                                     //Add Decimal part to the string
+    FP_string[i] = '.';
+    fpart = fpart * pow(10,afterpoint);
+  longToStr((long)fpart, FP_string + i + 1, afterpoint);}
+
+  Print_unrounded(FP_string, sign);
+  Round_for_Display(FP_string, sign, expt);
+
+return expt;}
+
+
+/**************************************************************************************************************************************/
+void Print_unrounded(char *FP_string, char sign){
+for(int m = 0; m <= 15; m++)un_rounded_array[m] = FP_string[m];
+if(sign == '-')
+    {for(int m = 0; m <= 14; m++)un_rounded_array[15-m] = un_rounded_array[14-m];               //For negative numbers shift the array once place to the right
+    un_rounded_array[0] = '-';}}
+
+
+
+/******************************************************************************************************/
+void Round_for_Display(char* res, char sign, signed char expt){
+   
+    int LSB_ptr;
+
+for (int m = 15; m; m--)                                                //Remove trailing zeros if the is a decimal point
+   {if (res[m] == 0) continue; 
+   if (res [m] == '0') res[m] = 0;
+   else break;}
+
+    
+    for (int m = 15; m; m--)                                            //start rounding with the least significant digit
+   {LSB_ptr = m;
+    if (res[m] == 0) continue; 
+   else  break;}
+
+if (res[LSB_ptr] >= '5'){res[LSB_ptr--] = 0; 
+if(res[LSB_ptr] == '.')LSB_ptr -= 1;
+res[LSB_ptr] += 1;
+while (res[LSB_ptr] == ':'){res[LSB_ptr--] = 0;
+if (res[LSB_ptr] == '.')LSB_ptr -= 1;
+res[LSB_ptr] += 1;}}
+
+if(sign == '-')
+    {for(int m = 0; m <= 15; m++)res[16-m] = res[15-m];               //For negative numbers shift the array once place to the right
+    res[0] = '-';}
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 /***************************************************************************************************************************************/
 long longToStr(long x, char str[], int d) 
@@ -227,5 +302,31 @@ f_number = *Flt_ptr_local;
 
 TWCR = (1 << TWINT);
 return f_number;}
+
+
+
+
+/**********************************************************************************************************/
+/* void ftoa(float Fnum, char FP_string[], int afterpoint){
+		long ipart;
+		char sign = '+';
+		
+		for(int m = 0; m <= 8; m++) FP_string[m] = 0;
+		
+		if (Fnum < 0){sign = '-'; Fnum *= (-1);}
+		
+		ipart = (long)Fnum;
+		float fpart = Fnum - (float)ipart;
+		long i = longToStr(ipart, FP_string, 0);
+
+		if (afterpoint != 0){
+			FP_string[i] = '.';
+			fpart = fpart * pow(10,afterpoint);
+		longToStr((long)fpart, FP_string + i + 1, afterpoint);}
+		
+		if(sign == '-'){for(int m = 0; m <= 7; m++)FP_string[8-m] = FP_string[7-m];
+		FP_string[0] = '-';	
+		}	}*/
+
 
 
