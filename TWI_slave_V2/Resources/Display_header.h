7ed985c6ce2f4e1f301390890e897c76e5b4dafd
aclandmark <mark.osborne15@btinternet.com>
2021-01-12 16:01:04 +0000

    Update Display_header.h

--- TWI_slave_V2/Resources/Display_header.h
+++ TWI_slave_V2/Resources/Display_header.h
@@ -41,11 +41,10 @@ if ((eeprom_read_byte((uint8_t*)(EE_size - 2)) > 0x0F)\
 == eeprom_read_byte((uint8_t*)(EE_size - 1)))) OSCCAL = eeprom_read_byte((uint8_t*)(EE_size - 2));
 
 
-/****************************************************************************************************/
-//DRIVERS for Most Significant Bits
+/****************************DRIVERS for Most Significant Bits of CA display*****************************/
 //IC on top of board
 //Hosts the Slave TWI
-
+/*
 #define	digit_3		PORTB |= (1 << PB6);
 #define	digit_2		PORTA |= (1 << PA6);
 #define	digit_1		PORTA |= (1 << PA5);
@@ -82,7 +81,7 @@ PORTB |= (seg_c | seg_d | seg_e | seg_g | DP);
 #define one_L 	PORTB &= ~(seg_c);
 #define ONE_L 	PORTB &= ~(seg_e);
 #define ONE		PORTB &= ~(seg_e); PORTB &= ~(seg_e);
-#define one 	PORTA &= ~(seg_b); PORTB &= ~(seg_c);		//(~(seg_b | seg_c));
+#define one 	PORTA &= ~(seg_b); PORTB &= ~(seg_c);
 #define two 	PORTA &= (~(seg_a | seg_b )); PORTB &= (~(seg_d | seg_e | seg_g));
 
 #define three 	PORTA &= (~(seg_a | seg_b)); PORTB &= (~(seg_c | seg_d | seg_g));
@@ -94,6 +93,8 @@ PORTB |= (seg_c | seg_d | seg_e | seg_g | DP);
 #define nine	PORTA &= (~(seg_a | seg_b | seg_f)); PORTB &= (~( seg_c | seg_g));
 #define zero	PORTA &= (~(seg_a | seg_b | seg_f )); PORTB &= (~( seg_c | seg_d | seg_e  ));
 #define minus	PORTB &= (~(seg_g));
+
+#define exponent PORTA &= (~(seg_a | seg_f)); PORTB &= (~(seg_d | seg_e | seg_g ));
 
 
 #define zero_point		zero; PORTB &= ~DP;
@@ -108,4 +109,76 @@ PORTB |= (seg_c | seg_d | seg_e | seg_g | DP);
 #define nine_point		nine;  PORTB &= ~DP;
 #define minus_point		minus; PORTB &= ~DP;
 
+*/
 
+/****************************DRIVERS for Most Significant Bits of CC display*****************************/
+//IC on top of board
+//Hosts the Slave TWI
+
+#define	digit_3		PORTB &= (~(1 << PB6));
+#define	digit_2		PORTA &= (~(1 << PA6));
+#define	digit_1		PORTA &= (~(1 << PA5));
+#define	digit_0		PORTB &= (~(1 << PB0));
+
+
+#define	seg_a 	(1 << PA3)
+#define	seg_b 	(1 << PA7)
+#define	seg_c 	(1 << PB4)
+#define	seg_d 	(1 << PB2)
+#define	seg_e 	(1 << PB1)
+#define	seg_f 	(1 << PA4)
+#define	seg_g 	(1 << PB5)
+#define	DP		(1 << PB3)
+
+
+#define set_digit_drivers \
+DDRA |= (1 << DDA3) | (1 << DDA4) | (1 << DDA5) | (1 << DDA6) | (1 << DDA7);\
+DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4) | (1 << DDB5) | (1 << DDB6);
+
+
+#define clear_digits	PORTB |= ((1 << PB0) | (1 << PB6)); \
+PORTA |= ((1 << PA5) | (1 << PA6));
+
+
+#define clear_display \
+PORTA &= (~(seg_a | seg_b | seg_f));\
+PORTB &= (~(seg_c | seg_d | seg_e | seg_g | DP));
+
+
+#define clear_display_buffer	for(int m = 0; m<=3; m++)display_buf[m] = 0;
+
+#define one_U 	PORTA |= seg_b;
+#define ONE_U 	PORTB |= seg_e;
+#define one_L 	PORTB |= seg_c;
+#define ONE_L 	PORTB |= seg_e;
+#define ONE		PORTB |= seg_e; PORTB |= seg_e;
+#define one 	PORTA |= seg_b; PORTB |= seg_c;	
+#define two 	PORTA |= (seg_a | seg_b ); PORTB |= (seg_d | seg_e | seg_g);
+
+#define three 	PORTA |= (seg_a | seg_b); PORTB |= (seg_c | seg_d | seg_g);
+#define four 	PORTA |= (seg_b | seg_f); PORTB |= (seg_c | seg_g);
+#define five 	PORTA |= (seg_a | seg_f ); PORTB |= (seg_c | seg_d | seg_g);
+#define six 	PORTA |= (seg_f); PORTB |= (seg_c | seg_d | seg_e | seg_g);
+#define seven 	PORTA |= (seg_a | seg_b); PORTB |= (seg_c);
+#define eight 	PORTA |= (seg_a | seg_b | seg_f);  PORTB |= (seg_c | seg_d | seg_e | seg_g);
+#define nine	PORTA |= (seg_a | seg_b | seg_f); PORTB |= ( seg_c | seg_g);
+#define zero	PORTA |= (seg_a | seg_b | seg_f ); PORTB |= ( seg_c | seg_d | seg_e  );
+#define minus	PORTB |= (~(seg_g));
+
+
+#define exponent PORTA |= (seg_a | seg_f); PORTB |= (seg_d | seg_e | seg_g );
+
+
+#define zero_point		zero; PORTA |= DP;
+#define one_point		one; PORTA |= DP;
+#define two_point		two;  PORTA |= DP;
+#define three_point		three;  PORTA |=DP;
+#define four_point		four;  PORTA |= DP;
+#define five_point		five;  PORTA |= DP;
+#define six_point		six;  PORTA |= DP;
+#define seven_point		seven;  PORTA |= DP;
+#define eight_point		eight;  PORTA |= DP;
+#define nine_point		nine;  PORTA |= DP;
+#define minus_point		minus; PORTA |= DP;
+
+/*********************************************************************************************************/
