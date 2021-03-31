59825bfc64a7f8b37ce10eef6880346b1dfb0e11
aclandmark <mark.osborne15@btinternet.com>
2021-03-31 09:46:35 +0100

    Update UNO_template_V2.h

--- UNO_template_V2/UNO_template_V2.h
+++ UNO_template_V2/UNO_template_V2.h
@@ -61,8 +61,8 @@ TWAR = 0x06;
 #define flash_on_FPN_overflow \
 \
 if(!(eeprom_read_byte((uint8_t*)0x3FB))){eeprom_write_byte((uint8_t*)(0x3FA),0);\
-float_num = float_num_from_eepom();\
-float_num_to_display(float_num);\
+float_num_1 = float_num_from_eepom();\
+float_num_to_display(float_num_1);\
 wdt_enable(WDTO_1S);\
 while(1){if(Serial.read()== 'a')\
 {eeprom_write_byte((uint8_t*)0x3FB, 0xFF);\
