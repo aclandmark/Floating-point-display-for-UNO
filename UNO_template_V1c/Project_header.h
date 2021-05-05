93994512862950915006451531b9ffc8dcf48c0b
aclandmark <mark.osborne15@btinternet.com>
2021-05-05 21:19:08 +0100

    Create Project_header.h

---
+++ UNO_template_V1c/Project_header.h
@@ -0,0 +1,5 @@
+
+#include "Resources_UNO_template\UNO_TWI_subroutines_V1c.c"
+#include "Resources_UNO_template\ASKII_subroutines_V1c.c"
+#include "Resources_UNO_template\PCI_subroutines_V1c.c"
+#include "Resources_UNO_template\UNO_template_header.h"
