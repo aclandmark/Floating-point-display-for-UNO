388023ffe485bd5a93ff3cb00c2b46bcbe76b354
aclandmark <mark.osborne15@btinternet.com>
2021-05-22 17:35:47 +0100

    Update Project.h

--- TWI_slave_V1/TWI_slave_V1/Project.h
+++ TWI_slave_V1/TWI_slave_V1/Project.h
@@ -6,13 +6,10 @@
 #include <avr/wdt.h>
 #include <avr/eeprom.h>
 #include <avr/pgmspace.h>
-//#include <util/delay.h>
 
 volatile char display_buf[4];
 
 #include "../Resources/Display_header.h"
-//#include "../Resources/USI_subroutines.c"
-//#include "../Resources/IO_subroutines.c"
 #include "../Resources/TWI_slave_subroutines.c"
 
 
